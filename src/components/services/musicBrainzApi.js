880093165376b97d0628121cf9fc3eb0a6947750
Zach Butler <zach.standley93@gmail.com>
2019-08-21 16:07:53 -0700

    made function called get albums that fetches albums from api

--- src/components/services/musicBrainzApi.js
+++ src/components/services/musicBrainzApi.js
@@ -6,7 +6,21 @@ export const getArtists = (name) => {
     }
   })
     .then(res => {
-      if(!res.ok) throw 'Could not save note';
+      if(!res.ok) throw 'Could not get artists';
+
+      return res.json();
+    });
+};
+
+export const getAlbums = (id) => {
+  return fetch(`http://musicbrainz.org/ws/2/release?artist=${id}&fmt=json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(res => {
+      if(!res.ok) throw 'Could not get albums';
 
       return res.json();
     });
