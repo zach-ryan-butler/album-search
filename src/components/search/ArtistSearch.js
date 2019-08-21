8e55146446c322883df2fa1c48c3f21688a29c3f
Zach Butler <zach.standley93@gmail.com>
2019-08-21 12:58:53 -0700

    added button to search form

--- src/components/search/ArtistSearch.js
+++ src/components/search/ArtistSearch.js
@@ -9,6 +9,7 @@ function ArtistSearch({ text, textHandler, submitHandler }) {
         value={text} 
         placeholder="search.." 
         onChange={textHandler} /> 
+      <button>Search</button>
     </form>
   );
 } 
