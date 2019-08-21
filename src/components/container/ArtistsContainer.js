7468504549f759892fc2e0ef25ecfff4e77d9a82
Zach Butler <zach.standley93@gmail.com>
2019-08-21 16:11:44 -0700

    made artist container

--- src/components/container/ArtistsContainer.js
+++ src/components/container/ArtistsContainer.js
@@ -1,17 +1,39 @@
 import React, {  Component } from 'react';
-import Artists from '../artist/Artists';
+import List from '../common/List';
+import ArtistSearch from '../search/ArtistSearch';
+import Artist from '../artist/Artist';
+import { getArtists } from '../services/musicBrainzApi';
 
 export default class ArtistsContainer extends Component {
   state = {
+    text: '',
     artists: []
   }
 
+  textHandler = ({ target }) => {
+    this.setState({ text: target.value });
+  }
+
+  submitHandler = event => {
+    event.preventDefault();
+    getArtists(this.state.text)
+      .then(res => {
+        this.setState({ artists: res.artists });
+      })
+      .catch(console.log('we got issues'));
+  }
   
 
 
   render() {
     return (
-      <Artists artists={this.state.artists}/>
+      <>
+        <ArtistSearch 
+          text={this.state.text} 
+          textHandler={this.textHandler} 
+          submitHandler={this.submitHandler} />
+        <List arr={this.state.artists} Comp={Artist} />  
+      </>
     );
   }
 }
