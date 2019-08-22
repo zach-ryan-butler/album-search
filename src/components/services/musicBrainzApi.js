824f8fe2c600597c6c92d2ab57904d8d65825aed
Zach Butler <zach.standley93@gmail.com>
2019-08-22 16:59:31 -0700

    massaged data to include totalpages

--- src/components/services/musicBrainzApi.js
+++ src/components/services/musicBrainzApi.js
@@ -1,5 +1,5 @@
-export const getArtists = (name) => {
-  return fetch(`http://musicbrainz.org/ws/2/artist?query=${name}&fmt=json&limit=25`, {
+export const getArtists = (name, offset) => {
+  return fetch(`http://musicbrainz.org/ws/2/artist?query=${name}&fmt=json&limit=25&offset=${offset}`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json'
@@ -9,11 +9,18 @@ export const getArtists = (name) => {
       if(!res.ok) throw 'Could not get artists';
 
       return res.json();
-    });
+    })
+    .then(json => ({
+      artists: json.artists,
+      count: json.count,
+      created: json.created,
+      offset: json.offset,
+      totalPages: Math.ceil(json.count / 25)
+    }));
 };
 
-export const getAlbums = (id) => {
-  return fetch(`http://musicbrainz.org/ws/2/release?artist=${id}&fmt=json`, {
+export const getAlbums = (id, offset) => {
+  return fetch(`http://musicbrainz.org/ws/2/release?artist=${id}&fmt=json&limit=25&offset=${offset}`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json'
@@ -23,7 +30,13 @@ export const getAlbums = (id) => {
       if(!res.ok) throw 'Could not get albums';
 
       return res.json();
-    });
+    })
+    .then(json => ({
+      ['release-count']: json['release-count'],
+      ['release-offset']: json['release-offset'],
+      releases: json.releases,
+      totalPages: Math.ceil(json['release-count'] / 25)
+    }));
 };
 
 export const getSongs = (id) => {
