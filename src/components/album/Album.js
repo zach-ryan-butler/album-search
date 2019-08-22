c32ba2643119503ad43dd7a07dc47c08be98d2e5
Zach Butler <zach.standley93@gmail.com>
2019-08-22 16:56:53 -0700

    changed name of prop from "prop" to "obj", prop is reserved

--- src/components/album/Album.js
+++ src/components/album/Album.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Album.css';
 import { Link } from 'react-router-dom';
 
-function Album({ props }) {
+function Album({ obj }) {
+  const coverArt = obj['cover-art-archive'].count ? `http://coverartarchive.org/release/${obj.id}/front` : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCe_V81Br1ClM5ezpV5aJkoq23Y1ecEcz-5b68AAfPE9Xw95w5Gg';
   return (
-    <section>
-      <Link to={`/songs/${props.name}/${props.title}/${props.id}`}>{props.title}</Link>
+    <section className={styles.Album}>
+      <Link to={`/songs/${obj.name}/${obj.title}/${obj.id}`}>{obj.title}</Link>
+      <img src={coverArt} />
     </section>
   );
 }
 
 Album.propTypes = {
-  props: PropTypes.object.isRequired,
-  name: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  id: PropTypes.string.isRequired
+  obj: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    ['cover-art-archive']: PropTypes.shape({
+      count: PropTypes.number.isRequired
+    }).isRequired
+  }).isRequired
 };
 
 export default Album;
